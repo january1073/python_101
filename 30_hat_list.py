dd986a411b92445e7a417374819df0cf5f9d6cbb
january1073 <185717411+january1073@users.noreply.github.com>
2025-03-05 17:31:14 +0100

    Add files via upload

---
+++ 30_hat_list.py
@@ -0,0 +1,10 @@
+# Hat list
+hat_list = [1, 2, 3, 4, 5]
+
+hat_list[2] = int(input("Enter an integer number: "))
+
+del hat_list[-1]
+
+print(len(hat_list))
+
+print(hat_list)
---
+++ 31_the_beatles.py
@@ -0,0 +1,23 @@
+# Append and insert members of the Beatles
+
+beatles = []
+print("Step 1:", beatles)
+
+beatles.append("John Lennon")
+beatles.append("Paul McCartney")
+beatles.append("George Harrison")
+print("Step 2:", beatles)
+
+for i in range (len(beatles)-2):
+      beatles.append("Stu Sutcliffe")
+      beatles.append("Pete Best") 
+print("Step 3:", beatles)
+
+del beatles[4]
+del beatles[3]
+print("Step 4:", beatles)
+
+beatles.insert(0, "Ringo Starr")
+print("Step 5:", beatles)
+
+print("The Fab", len(beatles))
